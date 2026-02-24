f3c57b80136b69d47a76f3e2abae5457f2932f00
Aleksandar Ristic <storiaca@gmail.com>
2026-02-24 16:05:03 +0100

    Setup years for horoscope

--- practice_lab_book/ch02_chatbots/04_horoscope_bot.py
+++ practice_lab_book/ch02_chatbots/04_horoscope_bot.py
@@ -1,5 +1,37 @@
 # Horoscope Bot
-# Author: Alekssandar Ristic
+# Author: Aleksandar Ristic
 # Date: 23.02.2026.
 # Description: The bot asks what year you are
 # born and tells you your Chinese Zodiac sign.
+
+# Greet the user
+print("Hello! I’m the Horoscope Bot. \
+Tell me, what year were you born?")
+
+# Get users year of birth
+birth_year = input()
+
+# Rat - 1996, 1984, 1972, 1960, 1948
+
+# Ox - 1997, 1985, 1973, 1961, 1949
+
+# Tiger - 1998, 1986, 1974, 1962, 1950
+
+# Rabbit - 1999, 1987, 1975, 1963, 1951
+
+# Dragon - 2000, 1988, 1976, 1964, 1952
+# Snake - 2001, 1989, 1977, 1965, 1953
+# Horse - 2002, 1990, 1978, 1966, 1954
+# Goat - 2003, 1991, 1979, 1967, 1955
+# Monkey - 2004, 1992, 1980, 1968, 1956
+# Rooster - 2005, 1993, 1981, 1969, 1957
+# Dog - 2006, 1994, 1982, 1970, 1958
+# Pig - 2007, 1995, 1983, 1971, 1959
+
+
+
+
+
+
+
+
