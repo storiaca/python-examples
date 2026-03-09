3afb5bf20e2526c9a0de4c005bf36a2f7fd5f5ec
Aleksandar Ristic <storiaca@gmail.com>
2026-03-09 19:06:32 +0100

    Bubble tea menu

---
+++ practice_lab_book/ch02_chatbots/09_bubble_tea_meny.py
@@ -0,0 +1,12 @@
+# Bubble Tea Menu (Version 1)
+# Author: Alekssandar Ristic
+# Date: 09.03.2026.
+# Prints a bubble tea menu
+
+flavours = ["Mango", "Taro", "Matcha", "Oreo",
+"Strawberry", "Coconut"]
+
+for flavour in flavours:
+  print(flavour + " milk")
+  # print("~~~~~~~~~~~~~")
+  print("###############")
