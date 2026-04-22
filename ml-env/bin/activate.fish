a15600efb1987fbda6f645e453fe8bd4644fe9a1
Aleksandar Ristic <storiaca@gmail.com>
2026-04-22 18:34:11 +0200

    Week 1 day 3 start

--- ml-env/bin/activate.fish
+++ ml-env/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/aca/python-examples/ml-env
+set -gx VIRTUAL_ENV /home/stori/python-examples/ml-env
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
